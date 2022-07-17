06868839c61b4a61954287ed155543b859687dcc
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-17 13:59:53 +0300

    movieCard started

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const MovieCard = () => {
   return (
-    <div>MovieCard</div>
+    <div>
+        
+    </div>
   )
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
 import MovieCard from '../components/MovieCard';
 import './Main.css';
 
 const Main = () => {
 
+    const [loading, setLoading] = useState(true);
+    const [movie,setMovie] = useState([]);
 
+
+    // const APP_KEY = process.env.REACT_APP_KEY;
+
+    // const url = `https://api.themoviedb.org/3/movie/550?api_key=${APP_KEY}`
     const url = `https://api.themoviedb.org/3/movie/550?api_key=9c93618c90ee9fcfe38f6994e743d164`
 
-    // const getMovie = async () => {
-    //     const {data} = await axio
-    // } 
+    const getMovie = async () => {
+        try {
+            const {data} = await axios.get(url);
+            setMovie(data);
+            console.log(data);
+            setLoading(false);  
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+    
+    useEffect(() => {
+        getMovie()
+    }, [])
+
+    if(loading){
+        return <h1>Loading...</h1>
+      }
+    
 
   return (
     <div className='mainDiv'>
@@ -19,7 +42,12 @@ const Main = () => {
             </form>
         </section>
         <div className='movieCardDiv'>
-            <MovieCard/>
+            <div className="filmCard">
+                <MovieCard/>
+            {/* {movie.map(() => {
+                return (<MovieCard/>)
+            })} */}
+            </div>
         </div>
     </div>
   )
