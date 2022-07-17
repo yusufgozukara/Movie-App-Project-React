8429dbb3701b1f00cc166c79074129524822ccab
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-17 12:45:26 +0300

    api key and axios added

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import MovieCard from '../components/MovieCard';
 import './Main.css';
 
 const Main = () => {
+
+
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=9c93618c90ee9fcfe38f6994e743d164`
+
+    // const getMovie = async () => {
+    //     const {data} = await axio
+    // } 
+
   return (
     <div className='mainDiv'>
         <section className='searchSection'>
@@ -9,7 +18,9 @@ const Main = () => {
                 <input className='searchMovie' type="text" placeholder='Search a movie' />
             </form>
         </section>
-        
+        <div className='movieCardDiv'>
+            <MovieCard/>
+        </div>
     </div>
   )
 }
