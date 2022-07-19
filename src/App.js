917a52867fb52b57d0ca35290d0a5f115c223c7e
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-19 09:15:29 +0300

    firebase folder and some func added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+
 import Router from './router/Router';
 
 function App() {
--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,14 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration at project settings part
-// See: https://firebase.google.com/docs/web/learn-more#config-object
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // ...
+  apiKey: "AIzaSyAb7aEkiNBMKzsp7Zj45C_zeOMAdmblPz4",
+  authDomain: "movie-app-f944a.firebaseapp.com",
+  projectId: "movie-app-f944a",
+  storageBucket: "movie-app-f944a.appspot.com",
+  messagingSenderId: "515808304075",
+  appId: "1:515808304075:web:8b9161773999d48ca1b44e",
+  measurementId: "G-4FZ6E32S0S"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+export const auth = getAuth(app);
+export default app;
